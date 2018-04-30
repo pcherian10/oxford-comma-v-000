8bdfe0ee63799ee39073ea1e7bb37b4e2c3a4e25
Paul <pcherian10@gmail.com>
2018-04-30 20:13:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  return array[0] if array.length == 1
+  string = ""
+  array.each.with_index do |var, i|
+    return "#{var} and #{array[i + 1]}" if (array.length == 2)
+    string += "#{var}, " if(var != array[-1])
+    string += "and #{var}" if(var == array[-1])
+  end
+  string
 end
+
+oxford_comma(["kiwi"])
