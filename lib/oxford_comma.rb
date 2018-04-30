358dbc73cb34e916291afcac40cf198ed843ac89
Paul <pcherian10@gmail.com>
2018-04-30 19:49:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
   arr = array.split(",")
-  return arr if arr.length = 1
+  return arr if arr.length == 1
   
   arr.each.with_index do |var, i|
-    return var if 
+    if(arr.length == 2)
+      return "#{var[i]" and "#{var}[]
   
 
 
