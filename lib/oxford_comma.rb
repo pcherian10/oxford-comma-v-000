064cc7d8db403a97c25adf04410664ea57371736
Paul <pcherian10@gmail.com>
2018-04-30 20:02:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   return array if array.length == 1
+  string = " "
   array.each.with_index do |var, i|
-    string = " "
     return "#{var} and #{var[i + 1]}" if (array.length == 2)
     string += "#{var},"
     string += "and #{var}" if(var == array[-1])
